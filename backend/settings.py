17cd85a50f0fb941a1d5db28adf220a0618e2c8a
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-19 22:55:02 +0100

    Added User App with Custom User Model, Applied migrations

--- backend/settings.py
+++ backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.customuser'
